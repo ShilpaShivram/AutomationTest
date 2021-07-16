4595e618de4e92fa502bc182547ee4e38143c544
ShilpaShivram <shilpa.rao80@gmail.com>
2021-07-16 21:39:52 +1000

    Automation Excercise1

--- Nine/src/test/java/Automation/Nine/Base.java
+++ Nine/src/test/java/Automation/Nine/Base.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Base {
 	public static WebDriver driver;
 	public void initialize() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sss\\cromedriver\\chromedriver.exe");
+		String projPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projPath+"/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.afr.com/policy/foreign-affairs/capability-edge-and-keeping-south-china-sea-open-crucial--christopher-pyne-20180924-h15rq9");
